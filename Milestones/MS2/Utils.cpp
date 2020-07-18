e9fc45be776fe5b79ef6c4b91b448174c210d23c
sean <ramoletesean28@gmail.com>
2020-07-17 23:50:19 -0400

    oo

---
+++ Milestones/MS2/Utils.cpp
@@ -0,0 +1,10 @@
+// Sean Ramolete
+// 155074198
+
+#include <iostream>
+#include "Utils.h"
+
+namespace sdds
+{
+
+}
---
+++ Milestones/MS2/Utils.h
@@ -0,0 +1,12 @@
+// Sean Ramolete
+// 155074198
+
+#ifndef SDDS_UTILS_H
+#define SDDS_UTILS_H
+
+namespace sdds
+{
+
+}
+
+#endif // SDDS_UTILS_H
